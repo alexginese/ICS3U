64c4622613b7c39806c6848f9887ef668bee9a26
alexginese <alex.ginese@stu.ocsb.ca>
2024-04-04 14:15:19 -0400

    a

---
+++ Unit01/1.1-1.5github/1.3-Mathematics.js
@@ -0,0 +1,28 @@
+// Part 1
+//Create variables for celcius and fahrenheint
+let celsius = 100;
+let fahrenheit = 0;
+//Using equation to change celcius to fahrenheint
+fahrenheit = (celsius * 9/5) + 32;
+//Output fahrenheint
+console.log(fahrenheit)
+//Part 2
+//change fahrenehit value to 77
+fahrenheit = 77;
+////Using equation to change fahrenheint to celcius
+celsius = (fahrenheit - 32) * 5/9;
+//Output celcius
+console.log(celsius);
+//Part 3
+//Output using celcius variabley
+console.log("77 degrees Fahrenheit is",celsius,"degrees celsius.");
+//Last Part
+//Create variable
+x1 = -4;
+y1 = 5;
+x2 = 6;
+y2 = 0;
+//Use equation
+m = (y2 - y1)/(x2 - x1);
+//print answer
+console.log("The slope is equal to",m);
