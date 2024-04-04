d01ddd0f550298d4ae6a8134b925f13da3509413
alexginese <alex.ginese@stu.ocsb.ca>
2024-04-04 14:06:06 -0400

    a

---
+++ Unit01/1.1-1.5github/1.2-Variables.js
@@ -0,0 +1,15 @@
+//Part 1
+//Creating variables for lenght and width
+let length = 10;
+let width = 5;
+//Creating area variable
+let area =  length * width;
+//Print area
+console.log("Area:",area);
+//Part 2
+//Creating variable for first and last name
+let first_name = "Santa";
+var last_name = "Claws";
+var last_name = "Claus";
+//Output using the variables created
+console.log("I still believe in",first_name,last_name+".")
