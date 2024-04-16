0f78ccb5521d627d5e375c73a5c44dbc5d17f13c
alexginese <159183786+alexginese@users.noreply.github.com>
2024-04-16 13:00:10 -0400

    1.5 Conditionals2.js

--- Unit01/1.1-1.5github/1.5 Conditionals2.js
+++ Unit01/1.1-1.5github/1.5 Conditionals2.js
@@ -4,7 +4,7 @@ let userName = prompt("What is your name? ");
 let age = parseInt(prompt("Hello" + userName +", how old are you?"));
 // Check age
 if (age >= 16) {
-    console.log("You are old enough to drive. Go crazy.");
+    console.log("You are old enough to drive.");
 }
 else {
     console.log("You are not old enough to drive yet.");
